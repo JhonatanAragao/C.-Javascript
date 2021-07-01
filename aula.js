96be0f12cf76c440082e806e624af02fe2ea95f3
Jhonatan Aragão Costa <82183586+JhonatanAragao@users.noreply.github.com>
2021-06-30 21:18:32 -0300

    Add files via upload

---
+++ aula.js
@@ -0,0 +1,80 @@
+// console.log ('Jhonatan')
+// console.log ("bem vindo ao curso")
+// let idade = 5;
+// console.log(idade)
+// let altura = 180
+// console.log(altura)
+// let valorIngressoAdulto = 20
+// valorIngressoAdulto = 30
+// console.log (valorIngressoAdulto);
+
+//**TIPOS PRIMITIVOS:***
+
+//**VALORES (String, number, boolean, underfined e null):
+    //let nome = "Rafael"; //STRING literal  - qualquer caractere
+    //let idade = 25; //NUMBER literal - qualuqer valor de número
+   // let estaaprovado = true; //BOOLEAN - define se está ou não / verdadeiro ou falso / ligado ou desligado;
+   // let sobrenome = undefined; //UNDERFINED - variável sem valor definido
+    //let corSelecionada = null; //NULL - Redefinir valor - usado em casos que voce queira resetar o sistema; 
+    // Exemplo do Null: Ao chegar no site tem um campo para selecionar a cor e o usuário escolhe a cor azul
+    // quando outra pessoa entrar no site, o valor da variável retorna para null permitindo que o novo usuário possa fazer também sua escolha;
+
+//**REFERENCIA (Objetos, Arrays e funções):
+
+//**OBJETO */
+
+    // let nome = "Rafael";
+    // let idade = 25;
+    // let estaaprovado = true; 
+    // let sobrenome = undefined; 
+    // let corSelecionada = null;
+
+    //AO CHAMAR UMA VARIÁVEL RECEBENDO OUTRAS VARIÁVEIS, TEMOS A FORMAÇÃO  DE UM OBJETO
+
+    // let pessoa = {
+    //     nome: "Rafael",
+    //     idade: 25,
+    //     estaaprovado: true,
+    //     sobrenome: "de sousa"
+    // };
+    // console.log(pessoa);
+
+//ARRAY */
+
+// let familia = [26,45,"jhonatan",50,2];
+// console.log(familia.length);
+// console.log(familia[2]);;
+//ex:
+// let nomeDoColega = ["Jhonatan", 22, "Teresina", "Masculino"]; //nome idade e cidade
+// console.log(nomeDoColega.length);
+// console.log(nomeDoColega);
+
+//FUNÇÃO */
+
+// Nomear função: verbo + substantivo
+
+//EX 1:
+// let corSite = "Azul";
+// function resetaCor (){
+//      corSite = "";
+// };
+
+// console.log(corSite)
+// resetaCor();
+// console.log(corSite)
+
+//EX 2: 
+
+// let corSite = "Vermelho";
+// console.log(corSite);
+
+// function alterarCor (cor,tonalidad) {
+//     corSite = cor + tonalidad + " " + "jhonatan";
+// };
+
+// alterarCor ("Verde"," Peixerman");
+// console.log(corSite);
+
+
+//PAREI EM 38:53
+//https://youtu.be/i6Oi-YtXnAU
