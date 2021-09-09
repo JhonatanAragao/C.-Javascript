6de3a48e6e4958a45c6b7e0c837c0844de93e1ce
JhonatanAragao <ajhonatamslash@gmail.com>
2021-09-08 21:02:19 -0300

    condicionais compostas e aninhadas

---
+++ condicoes parte1.js
@@ -0,0 +1,16 @@
+// var vel = 89;
+// console.log(`A velocidade do seu carro é ${vel}Km/h!`)
+// if (vel > 60){
+//     console.log('Você ultrapassou o limite de velocidade. MULTADO!')
+// }
+// console.log('Dirija sempre usando o cinto de segurança!')
+
+//EXEMPLO 2: Mostrar se a pessoa for do brasil = brasileiro, se não for, mostrar estrangeiro!
+
+let pais = prompt("digite seu pais");
+if (pais != "Brasil") {
+    console.log("Você é estrangeiro")
+} 
+else {
+    console.log("Você é Brasileiro")
+}
