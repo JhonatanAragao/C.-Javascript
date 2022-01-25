05c30fd4418f1ee98eb249a53ce4b731c9e72062
JhonatanAragao <ajhonatamslash@gmail.com>
2022-01-24 21:11:34 -0300

    exercicios

---
+++ Repetições/while.js
@@ -0,0 +1,19 @@
+//var a = parseInt(10);
+//while (a < 100){
+    //console.log(a)
+    //a++
+
+//}
+//let n = 0;
+//while (n <= 5) {
+//    console.log(n)
+//    n++
+//}
+let A = new Array ();
+A = ["Gol", "Uno", "S10", "Kadett"];
+let i = 0;
+while (i < A.length){
+    console.log(A[i])
+    i++
+}
+
